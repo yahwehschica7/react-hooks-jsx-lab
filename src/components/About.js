78e575bda7ffe82a3b9896af3fb88ddeb957736a
Jessica Nelson <yahwehschica@yahoo.com>
2022-01-14 08:02:31 -0500

    commit tree c050b81ecd189b203e8db0f9e3ebc5c1cab3ab52

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+      <p>I like books and coffee.</p>
+      <img src={ image } alt="I made this"/>
+  </div>
+  // return <div>About</div>;
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+ return <nav>
+    <a href ="#home">Home</a>
+    <a href ="#about">About</a>
+  </nav>
+  // return <nav>NavBar</nav>;
 }
 
 export default NavBar;
